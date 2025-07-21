3a6d883fd7cc1ff33727c409d5985c30eefe5580
[Karla Lewis] <[karlaoglivie@gmail.com]>
2025-07-21 16:42:07 -0700

    Cleaning up signup.jsx

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -1,16 +1,3 @@
-// export default function Signup(){
-//   return (
-//    <div className="bg-gray-100 flex items-center justify-center min-h-screen">
-//       <div className="max-w-md w-full bg-white p-6 rounded-lg shadow-md">
-//         <h2 className="text-2xl font-bold mb-6 text-center">Signup</h2>
-//           {/* Render SignUpAlert here... */}
-//           {/* Render SignupForm here... */}
-//         <p className="mt-4 text-center text-sm">Have an account? <Link to="/login" className="text-blue-500 hover:underline">Login</Link></p>
-//       </div>
-//     </div> 
-//   )
-// }
-
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
@@ -48,7 +35,8 @@ export default function Signup() {
     <div className="bg-gray-100 flex items-center justify-center min-h-screen">
       <div className="max-w-md w-full bg-white p-6 rounded-lg shadow-md">
         <h2 className="text-2xl font-bold mb-6 text-center">Signup</h2>
-
+         {/* Render SignUpAlert here... */}
+          {/* Render SignupForm here... */}
         <SignupAlert alert={alert} showAlert={showAlert} />
         <SignupForm
           register={register}
