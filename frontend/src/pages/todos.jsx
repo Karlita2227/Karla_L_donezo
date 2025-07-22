38ee0ce0856c5a1d2653a584a65898ab5938dab1
[Karla Lewis] <[karlaoglivie@gmail.com]>
2025-07-22 16:53:08 -0700

    Creating the Todos List and Form

--- frontend/src/pages/todos.jsx
+++ frontend/src/pages/todos.jsx
@@ -2,20 +2,26 @@ import { useRef } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 
-const modalRef = useRef();
-
-const toggleNewTodoModal = () => {
-  // Check if the modal is currently open by accessing the `open` property of `modalRef`.
- if (modalRef.current.open) {
-  // If the modal is open, close it by calling the `close()` method.
-  modalRef.current.close();
- } else {
-	// If the modal is not open, open it by calling the `showModal()` method.
-  modalRef.current.showModal();
- }
-}
-
 export default function Todos(){
+  const modalRef = useRef();
+  const { register, handleSubmit} = useForm();
+
+  const toggleNewTodoModal = () => {
+    // Check if the modal is currently open by accessing the `open` property of `modalRef`.
+   if (modalRef.current.open) {
+    // If the modal is open, close it by calling the `close()` method.
+    modalRef.current.close();
+   } else {
+    // If the modal is not open, open it by calling the `showModal()` method.
+    modalRef.current.showModal();
+   }
+  }
+
+  const onSubmit = (data) => {
+    console.log("Creating todo:", data);
+    modalRef.current.close(); // Optionally close modal
+  };
+ 
   return (
     <button className="btn btn-primary" onClick={() => toggleNewTodoModal()}>(
      <dialog ref={modalRef}_className="modal">
@@ -46,7 +52,7 @@ export default function Todos(){
               <button type="submit" className="btn btn-primary">
                 Create Todo
               </button>
-              <button type="button" className="btn btn-ghost">
+              <button type="button" className="btn btn-ghost" onClick={toggleNewTodoModal}>
                 Close
               </button>
             </div>
